e5f9ac6ad7bd5c782a528b7b9b2316646aa771ce
Samat Abibulla <54584147+a-samat-dev@users.noreply.github.com>
2026-07-14 19:42:16 +0500

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.kt
@@ -0,0 +1,37 @@
+class Solution {
+    fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
+        val inWeightMap = mutableMapOf<Int, Int>()
+        val outEdgesMap = mutableMapOf<Int, LinkedList<Int>>()
+        val result = LinkedList<Int>()
+
+        for (i in 0 until numCourses) {
+            inWeightMap[i] = 0
+            outEdgesMap[i] = LinkedList<Int>()
+        }
+
+        for (edge in prerequisites) {
+            inWeightMap[edge[0]] = inWeightMap[edge[0]]!!.plus(1)
+            outEdgesMap[edge[1]]?.add(edge[0])
+        }
+
+        val queue = LinkedList<Int>()
+        queue.addAll(inWeightMap.entries.filter { it.value == 0 }.map { it.key })
+        val completed = HashSet<Int>()
+
+        while (queue.isNotEmpty()) {
+            val poll = queue.poll()
+            if (completed.contains(poll)) continue
+            completed.add(poll)
+            result.add(poll)
+            val edges = outEdgesMap[poll]!!
+
+            for (edge in edges) {
+                inWeightMap[edge] = inWeightMap[edge]!!.minus(1)
+                if (inWeightMap[edge] == 0)
+                    queue.add(edge)
+            }
+        }
+
+        return if (completed.size == numCourses) result.toIntArray() else intArrayOf()
+    }
+}
