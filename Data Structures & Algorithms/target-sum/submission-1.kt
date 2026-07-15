fcd9d97dda304c05dee9f1ebac25703f62617dcc
Samat Abibulla <54584147+a-samat-dev@users.noreply.github.com>
2026-07-15 10:52:06 +0500

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.kt
@@ -0,0 +1,20 @@
+
+class Solution {
+
+    private var memo = mutableMapOf<String, Int>()
+
+    fun findTargetSumWays(nums: IntArray, target: Int): Int {
+        return findTargetSumWays(nums, target, 0, 0)
+    }
+
+    fun findTargetSumWays(nums: IntArray, target: Int, idx: Int, currSum: Int): Int {
+        if (idx >= nums.size) {
+            if (currSum == target) return 1
+            return 0
+        }
+        val key = "$idx - $currSum"
+        if (memo.contains(key)) return memo[key]!!
+        return findTargetSumWays(nums, target, idx + 1, currSum + nums[idx]) +
+                findTargetSumWays(nums, target, idx + 1, currSum - nums[idx])
+    }
+}
